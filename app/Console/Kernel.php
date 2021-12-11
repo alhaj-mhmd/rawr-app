240ee38ce0cda5cd827dcc68b1dbd85ac749f8b7
Mohammad <alhaj.mhmd@gmail.com>
2021-12-11 13:52:11 +0800

    Initial commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+
+        $schedule->call(function () {
+            DB::insert('INSERT INTO `rewards` ( `user_id`, `reward`) SELECT referrals.referrer , (amount * tiers.reward / 100) AS referrer_reward FROM purchases INNER JOIN referrals ON referrals.user_id = purchases.user_id LEFT JOIN tiers on tiers.id = referrals.tier_id WHERE date BETWEEN ( CURRENT_DATE() - INTERVAL 1 DAY) AND CURRENT_DATE');
+        })->dailyAt('03:00');
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
